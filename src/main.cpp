3671ca5f7ddbfeb02ae34007113f51ea8ea1f9e5
ellensp <ellensp@hotmsil.com>
2021-01-05 22:47:30 +1300

    code cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -41,16 +41,7 @@
 
 
 #define FAN0 P2_03 //on skr 14
-
 #define LED_BUILTIN FAN0
-
-// Generally, you should use "unsigned long" for variables that hold time
-// The value will quickly become too large for an int to store
-unsigned long previousMillis = 0;        // will store last time LED was updated
-int ledstate = off;
-
-// constants won't change:
-const long interval = 1000;           // interval at which to blink (milliseconds)
 
 void setup() {
   usb_cdc_init();
@@ -83,32 +74,6 @@ void setup() {
   digitalWrite(Z_ENABLE_PIN   , LOW);
   digitalWrite(E0_ENABLE_PIN  , LOW);
   digitalWrite(E1_ENABLE_PIN  , LOW);
-}
-
-void OLDloop() {
-  unsigned long currentMillis = millis();
-
-  if (currentMillis - previousMillis >= interval && ledstate == off) {
-    // save the last time you blinked the LED
-    previousMillis = currentMillis;
-
-    // set the LED with the ledState of the variable:
-    UsbSerial.println("LED On");
-    digitalWrite(LED_BUILTIN, HIGH);
-    ledstate = on;
-  }
-
-  if (currentMillis - previousMillis >= interval && ledstate == on) {
-    // save the last time you blinked the LED
-    previousMillis = currentMillis;
-
-    // set the LED with the ledState of the variable:
-    UsbSerial.println("LED Off");
-    digitalWrite(LED_BUILTIN, LOW);
-    ledstate = off;
-  }
-
-  usb_cdc_idle();
 }
 
 void loop () {
